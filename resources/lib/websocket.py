a5dc880a2d2068c1a18c26814edee01f2130c0f5
David Heitman <dmheitman@gmail.com>
2019-12-15 23:25:01 -0700

    Fix use of enumerate()

--- resources/lib/websocket.py
+++ resources/lib/websocket.py
@@ -358,7 +358,7 @@ class ABNF(object):
         """
         _m = array.array("B", mask_key)
         _d = array.array("B", data.encode())
-        for i in enumerate(_d):
+        for i, _v in enumerate(_d):
             _d[i] ^= _m[i % 4]
         return _d.tostring()
 
