871d8256767035c29007c66401b5ab9b495bdb5e
David Heitman <loucipher@gallifrey.nonlocal>
2020-01-16 23:29:03 -0700

    Fix typo

--- resources/lib/watchdog/observers/inotify_c.py
+++ resources/lib/watchdog/observers/inotify_c.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import unicode_literals with_statement
+from __future__ import unicode_literals, with_statement
 from builtins import object
 import os
 import errno
