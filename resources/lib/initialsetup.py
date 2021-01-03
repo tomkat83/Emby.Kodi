8d34e66764beb4a441f06353e5366cd62e85dc99
croneter <croneter@gmail.com>
2021-01-03 17:17:47 +0100

    Improve some code

--- resources/lib/initialsetup.py
+++ resources/lib/initialsetup.py
@@ -280,7 +280,6 @@ class InitialSetup(object):
         }
         or None if unsuccessful
         """
-        server = None
         # If no server is set, let user choose one
         if not app.CONN.server or not app.CONN.machine_identifier:
             showDialog = True
