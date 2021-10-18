4d3e36fbdb275233e00b2555510ebfb2f8d8e103
croneter <croneter@users.noreply.github.com>
2021-10-18 09:40:07 +0200

    Add missing HTTP headers that Plex for Window's Plex Companion uses

--- resources/lib/clientinfo.py
+++ resources/lib/clientinfo.py
@@ -42,6 +42,8 @@ def getXArgsDeviceInfo(options=None, include_token=True):
         'X-Plex-Version': v.ADDON_VERSION,
         'X-Plex-Client-Identifier': getDeviceId(),
         'X-Plex-Provides': 'client,controller,player,pubsub-player',
+        'X-Plex-Protocol': '1.0',
+        'Cache-Control': 'no-cache'
     }
     if include_token and utils.window('pms_token'):
         xargs['X-Plex-Token'] = utils.window('pms_token')
