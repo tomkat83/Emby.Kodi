33e0a50fd6e176b4abb0c970779039e997757272
Croneter <croneter@users.noreply.github.com>
2024-03-02 08:01:17 +0100

    beta version bump 3.8.8

--- resources/lib/variables.py
+++ resources/lib/variables.py
@@ -104,7 +104,7 @@ COMPANION_PORT = int(_ADDON.getSetting('companionPort'))
 PKC_MACHINE_IDENTIFIER = None
 
 # Minimal PKC version needed for the Kodi database - otherwise need to recreate
-MIN_DB_VERSION = '3.8.5'
+MIN_DB_VERSION = '3.8.8'
 
 DB_VIDEO_VERSION = None
 DB_VIDEO_PATH = None
